70ff299033a017928cf9dae1093ec88c07695907
daijie <daijie@php.net>
2021-12-04 18:54:55 +0800

    perf: 能够跳过同一个hash的编译，降低负载

--- manual/schedule.php
+++ manual/schedule.php
@@ -36,7 +36,11 @@ $langs = [
 ];
 try {
     foreach($langs as $langCode) {
-    
+
+        if (shouldSkipBuild($langCode)) {
+            continue;
+        }
+
         ob_start();
         exec("php doc-base/configure.php --with-lang=${langCode} --output=doc-base/${langCode}.manual.xml", $result, $returnCode);
         $content = ob_get_clean();
@@ -56,10 +60,53 @@ try {
             // watch out!
             exec("rm -rf ${dest}");
         }
-        symlink("../../${docDir}/output-${langCode}/php-web", $dest);    
+        symlink("../../${docDir}/output-${langCode}/php-web", $dest);
+        $hash = getManualHash($langCode);
+        setManualHashLog($langCode, $hash);
     }
 } catch (\Exception $e) {
     echo $e->getMessage();
 } finally {
     unlink($lockFile);
+}
+
+
+// function createHashLog() {
+
+// }
+
+/**
+ * 是否应该跳过该手册编译
+ */
+function shouldSkipBuild($langCode) {
+    $previousHash = getManualHashLog($langCode);
+    $hash = getManualHash($langCode);
+    return $previousHash === $hash;
+}
+
+/**
+ * 获取当前的 hash
+ */
+function getManualHash($langCode) {
+    return file_get_contents("${langCode}/.git/refs/heads/master");
+}
+
+/**
+ * 获取之前的 hash
+ */
+function getManualHashLog($langCode) {
+    $hashLogFile = "hash-${langCode}.txt";
+    if (file_exists($hashLogFile)) {
+        $hash = file_get_contents("hash-${langCode}.txt");
+        return $hash;
+    } else {
+        return false;
+    }
+}
+
+/**
+ * 设置 hash log
+ */
+function setManualHashLog($langCode, $hash) {
+    file_put_contents("hash-${langCode}.txt", $hash);
 }
